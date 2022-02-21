fa56d2975774803cb9e8785239bb5baac6e4b8b5
keval101 <kevalvadhiya101@gmail.com>
2022-02-21 14:51:17 +0530

    add projects link

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -12,34 +12,38 @@ export class ProjectsComponent implements OnInit {
       image: '/assets/images/natours.png',
       detailText: 'This is a theme for a tourism company using pure CSS3 with creative animations. This project is from the advanced CSS course of Jonas Schmedtmann on Udemy.',
       tags: ['HTML5', 'CSS3', 'SCSS', 'JAVA SCRIPT'],
-      liveCode: '',
-      sourceCode: ''
+      liveDemo: 'https://tourdiscover.netlify.app',
+      sourceCode: 'https://github.com/keval101/Natours'
     },
     {
       image: '/assets/images/nexterss.png',
       detailText: 'This is a website for a real estate company called Nexter using pure CSS and SCSS.I built this theme when I took the advanced CSS course from Jonas Schmedtmann on Udemy.The goal of building this page was to master CSS Grid layout and to learn new responsive design techniques',
       tags: ['HTML5', 'CSS3', 'SCSS', 'JAVA SCRIPT'],
-      liveCode: '',
-      sourceCode: ''
+      liveDemo: 'https://nexterlivinghome.netlify.app',
+      sourceCode: 'https://github.com/keval101/Nexter'
     },
     {
       image: '/assets/images/trilloo.png',
       detailText: 'I took Jonas Schmedtmann\'s Udemy course: Advanced CSS and Sass to learn advanced CSS conceptsand techniques.This is the second project in the course, the Trillo website. Flexbox was a major component tothis site.',
       tags: ['HTML5', 'CSS3', 'SCSS', 'JAVA SCRIPT'],
-      liveCode: '',
-      sourceCode: ''
+      liveDemo: 'https://trillohotelchecking.netlify.app',
+      sourceCode: 'https://github.com/keval101/Trillo'
     },
     {
       image: '/assets/images/freepcgames.png',
       detailText: 'I have created this free pc game site with HTML, SCSS and use Angular framework. I have integrated FREE TO PLAY API in this project.',
       tags: ['HTML5', 'CSS3', 'SCSS', 'ANGULAR'],
-      liveCode: '',
-      sourceCode: ''
+      liveDemo: 'https://freepcgames.netlify.app',
+      sourceCode: 'https://github.com/keval101/GameSite'
     }
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openLink(link: string): void {
+    window.open(link);
   }
 
 }
